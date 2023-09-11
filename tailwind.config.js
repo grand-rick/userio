808ec6dd724800a89bc53e19be7e6e081d1316aa
Patrick Murimi <kabugap02@gmail.com>
2023-09-12 00:57:04 +0300

    [feat]: replace div with table

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,6 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
-    screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
-    },
     extend: {},
   },
   plugins: [],
