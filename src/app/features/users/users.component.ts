75a7c086ac6f8f336ee05a3225004cdc10adb83d
Patrick Murimi <kabugap02@gmail.com>
2023-09-11 05:04:58 +0300

    [chore]: more icon to display edit/delete buttons

--- src/app/features/users/users.component.ts
+++ src/app/features/users/users.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { UsersService } from 'src/app/shared/data-access/services/users.service';
 import { User } from 'src/app/shared/data-access/types/User';
 
@@ -7,12 +7,27 @@ import { User } from 'src/app/shared/data-access/types/User';
   templateUrl: './users.component.html',
   styleUrls: ['./users.component.scss']
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
 
   users$ = this.usersService.getUsers();
 
-  constructor(private usersService: UsersService) {}
+  constructor(private usersService: UsersService, private renderer: Renderer2) {}
 
+  // displayOptions(): void {
+  //   this.renderer.addClass(dropdownContent, 'block');
+  // }
+
+  ngOnInit(): void {
+    const dropdown = document.querySelector('.dropdown');
+
+    // dropdown?.addEventListener('click', () => {
+    //   const dropdownContent = document.querySelector('.dropdown-content');
+    //   dropdownContent?.classList.toggle('block');
+    //   dropdownContent?.classList.toggle('hidden');
+    // });
+    
+  }
+  
   editUser(user: User): void {}
 
   deleteUser(user: User): void {}
