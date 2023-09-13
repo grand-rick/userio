3117c013cedb64d9aa3657139116dcf2d9a50d35
Patrick Murimi <kabugap02@gmail.com>
2023-09-13 03:46:00 +0300

    [feat]: Leverage Daisy table class

--- src/app/features/users/users.module.ts
+++ src/app/features/users/users.module.ts
@@ -4,7 +4,6 @@ import { CommonModule } from '@angular/common';
 import { UsersRoutingModule } from './users-routing.module';
 import { UsersComponent } from './users.component';
 import { UsersListComponent } from './users-list/users-list.component';
-import { UserComponent } from './user/user.component';
 
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { EditUserModalModule } from 'src/app/shared/ui/edit-user-modal/edit-user-modal.module';
@@ -13,8 +12,7 @@ import { HeaderModule } from '../header/header.module';
 @NgModule({
   declarations: [
     UsersComponent,
-    UsersListComponent,
-    UserComponent
+    UsersListComponent
   ],
   imports: [
     CommonModule,
